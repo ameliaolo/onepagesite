72a6be50a0456813b071a1dc1cc64e110d568c97
ameliaolo <millieolo@outlook.com>
2023-10-26 00:12:59 +1100

    k clocking out fr

--- script.js
+++ script.js
@@ -16,3 +16,37 @@
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));*/
 
+/*
+const text = document.querySelector(".different");
+const strText = text.textContent;
+const splitText = strText.split("");
+text.textContent = "";
+for(let i = 0; i < splitText.length; i++) {
+    text.innerHTML += "<span>" + splitText[i] + "</span>";
+}
+
+let char = 0;
+let timer = setInterval(onTick, 50);
+
+function onTick()
+{
+    const span = text.querySelectorAll('span')[char];
+    span.classList.add('fade');
+    char++
+    if(char === splitText.length) {
+        complete();
+        return;
+    }
+}
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
+}
+*/
+
+var audio = new Audio ('./assets/powerUp.mp3');
+
+function PlaySound() {
+    onclick = document.getElementById('onepagesite/assets/powerUp.mp3').play();
+}
